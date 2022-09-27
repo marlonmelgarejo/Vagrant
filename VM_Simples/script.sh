bfae2a9bd8621affe96047e3c4da6f084f867d88
Marlon Melgarejo <marlonmelgarejo@gmail.com>
2022-09-27 12:08:26 -0400

    Alteracao Vagrantfile

--- VM_Simples/script.sh
+++ VM_Simples/script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 apt update
+sudo apt-get install net-tools -y
+sudp apt-get inatall tree -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker vagrant
