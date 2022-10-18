c8250e5a227b105d5b7085015dff7c46ec7d0d79
marlonmelgarejo <68469108+marlonmelgarejo@users.noreply.github.com>
2022-10-18 15:29:54 -0400

    add curl e jq

--- VM_Simples/script.sh
+++ VM_Simples/script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 apt-get update
 sudo apt-get install net-tools -y
+sudo apt-get install curl -y
 sudo apt-get install tree -y
+sudo apt-get install jq -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker vagrant
